3105aeb6a719433f43356a4d09c84832719cc3a5
Adian Tsosie <addve12@yahoo.com>
2019-01-15 16:35:57 -0700

    Added layout for Purpose, Audience and Goal.

---
+++ public_html/documentation/milestone-1.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset ="utf-8">
+		<title>Dinero Digital Concepts</title>
+	</head>
+	<body>
+		<h1>Purpose:</h1>
+		<p>
+
+		</p>
+		<h2>Audience:</h2>
+		<p>
+
+		</p>
+		<h3>Goal:</h3>
+		<p>
+
+		</p>
+	</body>
+
+</html>
