1110ec7f2accb1dca811e45c59d16a0717fc81a1
Adian Tsosie <addve12@yahoo.com>
2019-01-23 21:57:24 -0700

    Added layout for Purpose, Audience and Goal.

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -6,16 +6,41 @@
 	</head>
 	<body>
 		<h1>Purpose:</h1>
-		<p>
-
+		<p>Dinero Digital Concepts is a multimedia design firm thay specializes in graphic design, web development,
+			motion graphics, projection mapping and brand development.
 		</p>
 		<h2>Audience:</h2>
 		<p>
 
 		</p>
-		<h3>Goal:</h3>
+		<h2>Goal:</h2>
 		<p>
 
+		</p>
+
+		<h2>Persona</h2>
+		<p>
+			<ul>
+				<li><strong>Name:</strong></li>Colton Hoy
+				<li><strong>Age:</strong></li>27
+				<li><strong>Lives:</strong></li>Santa Fe, New Mexico
+				<li><strong>Job:</strong></li>Owner of a promotion company
+				<li><strong>Technology Used:</strong></li>2014 MacBool Pro 15, macOS High Sierra, iPhone Xs, Xfinity Internet,
+								Sprint Unlimited plan.
+				<li><strong>Behaviors and Attitudes:</strong></li>
+				<li><strong>Site Usage:</strong></li>Looking through design portfolio.
+				<li><strong>Goal:</strong></li>
+					<ul>
+						<li>To find a company that can help him to brand and develop his prodcution company</li>
+						<li>Wants to have a great open communication with firm and designers</li>
+					</ul>
+				<li><strong>Frustrations:</strong>
+						<ul>
+							<li>His been through many company webpages with lack of communication</strong></li>
+							<li></li>
+						</ul>
+				<li><strong>Name:</strong></li>
+		</ul>
 		</p>
 	</body>
 
