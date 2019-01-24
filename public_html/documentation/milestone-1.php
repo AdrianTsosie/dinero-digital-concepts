2c4202eefe540fc7df5b88e309b0a0867231169e
Adian Tsosie <addve12@yahoo.com>
2019-01-23 22:42:46 -0700

    Updated PWP

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -5,17 +5,21 @@
 		<title>Dinero Digital Concepts</title>
 	</head>
 	<body>
-		<h1>Purpose:</h1>
+		<div>
+			<img src="DDC Logo.png" alt="DDC Logo">
+		</div>
+
+		<h2>Purpose:</h2>
 		<p>Dinero Digital Concepts is a multimedia design firm thay specializes in graphic design, web development,
 			motion graphics, projection mapping and brand development.
 		</p>
 		<h2>Audience:</h2>
-		<p>
-
+		<p>From small business owners to large corporations. Anyone who is looking for any from of digital media to help
+			grow, promote and brand there company or idea.
 		</p>
 		<h2>Goal:</h2>
-		<p>
-
+		<p>We would like to create a one on one relationship with our clients to help them create the visions they have.
+			for their companies. To be a part of the development and change in a company's brand.
 		</p>
 
 		<h2>Persona</h2>
@@ -25,9 +29,19 @@
 				<li><strong>Age:</strong></li>27
 				<li><strong>Lives:</strong></li>Santa Fe, New Mexico
 				<li><strong>Job:</strong></li>Owner of a promotion company
-				<li><strong>Technology Used:</strong></li>2014 MacBool Pro 15, macOS High Sierra, iPhone Xs, Xfinity Internet,
-								Sprint Unlimited plan.
+				<li><strong>Technology Used:</strong></li>
+					<ul>
+						<li>2014 MacBool Pro 15</li>
+						<li>macOS High Sierra</li>
+						<li>iPhone Xs</li>
+						<li>Xfinity Internet</li>
+						<li>Sprint Unlimited plan.</li>
+					</ul>
 				<li><strong>Behaviors and Attitudes:</strong></li>
+					<ul>
+						<li>Has been to a few other design companies.</li>
+						<li>Has a style of art work he wants.</li>
+					</ul>
 				<li><strong>Site Usage:</strong></li>Looking through design portfolio.
 				<li><strong>Goal:</strong></li>
 					<ul>
@@ -36,11 +50,10 @@
 					</ul>
 				<li><strong>Frustrations:</strong>
 						<ul>
-							<li>His been through many company webpages with lack of communication</strong></li>
-							<li></li>
+							<li>His been through many company web pages with lack of communication</strong></li>
+							<li>Tried Fiver and wasn't happy with results.</li>
 						</ul>
-				<li><strong>Name:</strong></li>
-		</ul>
+			</ul>
 		</p>
 	</body>
 
