086dd5a65c2ed521b2df09e6649af171fc6635fb
Adian Tsosie <addve12@yahoo.com>
2019-02-05 23:29:00 -0700

    Added my milestone 2a content.

---
+++ public_html/documentation/milestone-2.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset ="utf-8">
+    <title>Phase 2a</title>
+</head>
+<body>
+    <h1>Content Strategy</h1>
+        <p>
+            Dinero Digital Concepts strives to set itself about from other firms by keep up with new design trends
+            and give a cutting-edge look that will make your company stand out. With a wide arrange of looks and style
+            we can design something for everyone. As the world is moving to mobile first applications, this site will
+            fallow case and have mobile responsiveness for phones or tablets.
+        </p>
+        <p>
+            With the main splash page there will be the logo, background pic, short video, and mission statement
+            about the company. All these elements will fade in with CSS animation. The video will be a 30 second video
+            going over the mission statement and vision of my company. There will be written a written version the mission
+            statement underneath the video along with the title of the section. At the bottom of this section there
+            will be photo examples that are clickable and will blow up to bigger version of the image.
+        </p>
+        <p>
+            In the services section there will be a description off all the types of services we offer. From Graphic
+            Design for print and digital use, Web Development and Design, Motion Graphics, and Video Mapping. Under the
+            descriptions be photo examples that are clickable and will blow up to bigger version of the image. All of
+            this fill fade-in with CSS on scroll
+        </p>
+        <p>
+            Next will be the past work section where I’ll have photos of my most recent work laid out in a grid that
+            the user can click on to make image blow them bigger. All this fill fade-in with CSS on scroll.
+        </p>
+        <p>
+            After the past work gallery there will be a testimonial section. This section will fade in on scroll and
+            feature some past clients and a comment about the company
+        </p>
+        <p>
+            My last section is a contact form, contact info and links to the company’s social media links. The form
+            will allow user to send me a message regarding future project inquires, pricing and specific info need for
+            them to make the decision to choose us for their digital art needs.
+        </p>
+        <p>
+            With this site we want to increase leads and market relations by focusing on content that highlights the
+            design work that makes us stand out. I believe that with a left and right side of the brain page design, the
+            user will enjoy the visit and get them to contact the company and use us for future projects.
+        </p>
+
+    <h1>Wire Frames</h1>
+        <h2>Mobile Design</h2>
+            <div>
+                <img src="DDC Phone.png" width="60%" height="60%" alt="DDC Phone">
+            </div>
+
+
+        <h2>Desktop/laptop Design</h2>
+            <div>
+                <img src="DDC Webpage.jpg" width="50%" height="50%" alt="DDC Desktop/Laptop">
+            </div>
+</body>
